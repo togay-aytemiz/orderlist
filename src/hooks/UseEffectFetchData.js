857586b57257b412aa1f02adb8dd090971745dce
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-04 14:05:54 +0300

    add fetchData

--- src/hooks/UseEffectFetchData.js
+++ src/hooks/UseEffectFetchData.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+const url = "http://api.github.com/users";
+
 const UseEffectFetchData = () => {
-  return <div>UseEffectFetchData</div>;
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+    console.log(users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <>
+      <h3>Github Users</h3>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default UseEffectFetchData;
