e534544564a41addf1ea7cca736c06164bffbb40
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-10 12:16:26 +0300

    add show-hide

--- src/hooks/ShowHide.js
+++ src/hooks/ShowHide.js
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+
 const ShowHide = () => {
-  return <div>ShowHide</div>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setShow((pre) => !pre);
+        }}
+      >
+        Show / Hide
+      </button>
+      {show && <Item />}
+    </>
+  );
 };
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>Window</h1>
+      <h2>Size: {size}</h2>
+    </div>
+  );
+};
+
 export default ShowHide;
